4a2adba485595ea89c1c39b7b5b5e29dd2a92eaf
savif <62864999+savif@users.noreply.github.com>
2020-05-21 21:28:00 -0500

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+add2<- function(x,y){
+        x+y
+}
+above10 <-function(x){       ##{contains the function}
+        use <- x>10
+        x[use]                ##[subset]
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+columnmean <- function(x, removeNA = TRUE){
+        nc <-ncol(x)
+        means <- numeric(nc)
+        for(i in nc){
+                means[i] <-mean(x[,i])
+        }
+        
+        means
 }
+
+x<- as.Date("2012-01-01")
+y<- strptime("9 Ja+zn 2011 11:34:21", "%d %b %Y %H:%M:%S")
+x-y
+x<-as.POSIXlt(x)
+x-y
+f<- function(x) {
+        g<- function(y){
+                y+z
+                
+        }
+        z<-4
+        x +g(x)
+}
+z<-10
+f(3)
+
+x<-5
+y<- if(x<3){
+        NA
+} else {
+        10
+}
+y
+
+x<-1:10
+if()
+
